fe54d0c2d41fc48587f08fa4eab60ca391c29487
Ryan Camoras <ryan@mellmo.com>
2014-03-03 18:17:16 -0800

    added license header

--- roambi-api-java-client/src/main/java/com/mellmo/roambi/api/model/Role.java
+++ roambi-api-java-client/src/main/java/com/mellmo/roambi/api/model/Role.java
@@ -1,3 +1,7 @@
+/**
+ * This sample code and information are provided "as is" without warranty of any kind, either expressed or implied, including
+ * but not limited to the implied warranties of merchantability and/or fitness for a particular purpose.
+ */
 package com.mellmo.roambi.api.model;
 
 import com.google.gson.JsonObject;
